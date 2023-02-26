ac4f080ed1a80d3bafb7b7abffb82e54dcb38b7e
Javier <javier@gmail.com>
2023-02-26 23:59:07 +0000

    Added comments table and model

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,6 +2,7 @@ class Project < ApplicationRecord
   enum status: { NotStarted: 0, InProgress: 1, OnHold: 2, Completed: 3 }
 
   belongs_to :user
+  has_many :comments
 
   validates :name, presence: true, length: { maximum: 50 }
   validates :description, length: { maximum: 2000 }
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_26_233113) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_26_235328) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text "body", null: false
+    t.bigint "user_id"
+    t.bigint "project_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["project_id"], name: "index_comments_on_project_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "projects", force: :cascade do |t|
     t.string "name", limit: 50, null: false
