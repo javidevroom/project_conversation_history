0edb0abe812a35f0a90dd192b30148598b7ec655
Javier <javier@gmail.com>
2023-02-27 23:39:00 +0000

    Changed comments and history order to reverse chronological

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -1,2 +1,9 @@
 module ProjectsHelper
+  def versions
+    PaperTrail::Version.where(item_type: 'Project', item_id: @project.id).order(created_at: :desc)
+  end
+
+  def comments
+    @project.comments.order(created_at: :desc)
+  end
 end
