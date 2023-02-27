4deacc2312c3dbe0769433b64bab20086ed46c05
Javier <javier@gmail.com>
2023-02-27 22:17:41 +0000

    Added seed data for user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+# User.create!(email: 'javier@gmail.com', password: 'password', password_confirmation: 'password')
+puts "Created user successfully!\nemail: javier@gmail.com\npassword: password"
